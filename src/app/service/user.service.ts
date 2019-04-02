3d7fafe489326861af1bb6e98bb1ac80238eeebf
shulich <48982660+shulich@users.noreply.github.com>
2019-04-03 01:51:08 +0300

    Add files via upload

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { User } from '../model/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  userList:  User[] = [
+    { id: 1, name: 'shulamit' },
+    { id: 2, name: 'chagit' },
+    { id: 3, name: 'chaya' },
+    { id: 4, name: 'gilad' },
+    { id: 5, name: 'rini' }
+  ];
+  constructor() {
+  }
+
+  loadUserList():  User[] {
+    return this.userList;
+  }
+}
