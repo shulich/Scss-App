8db3a7f8a52dc441d41a0108ddba6ec83de7dd87
shulich <48982660+shulich@users.noreply.github.com>
2019-04-03 21:47:03 +0300

    Add files via upload

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,25 +1,32 @@
 import { Injectable } from '@angular/core';
 import { User } from '../model/user';
+import { Subject, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  userList:  User[] = [
+
+  public userList: Subject<User[]> = new Subject<User[]>();
+
+  _userList:  User[] = [
     { id: 1, name: 'shulamit' },
     { id: 2, name: 'chagit' },
     { id: 3, name: 'chaya' },
     { id: 4, name: 'gilad' },
     { id: 5, name: 'rini' }
   ];
+
   constructor() {
+   
   }
 
   loadUserList():  User[] {
-    return this.userList;
+    return this._userList.sort((a, b) => a.name.localeCompare(b.name));;
   }
   
     addUser(newUser:User) {
-    this.userList.push(newUser);
+      this._userList.push(newUser);
+      this.userList.next(this.loadUserList())
   }
 }
